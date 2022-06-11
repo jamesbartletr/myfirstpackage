cf76eead4071948ab0cd1c9af2d540ee3e0399a7
jamesbartletr <james.bartlett.1993@icloud.com>
2022-06-11 16:13:46 +0100

    Correction to if statement

--- R/print_functions.R
+++ R/print_functions.R
@@ -7,7 +7,7 @@ print_hello_world <- function(){
 print_star_wars <- function(who = "Obi Wan Kenobi"){
   if (who == "Obi Wan Kenobi"){
     print("Hello There!")
-  } (who == "General Grievous"){
+  }else (who == "General Grievous"){
     print("Ah, General Kenobi!")
   }
 }
