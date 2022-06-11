667d8ad105d11a6f8f9c92891a0b3903d5fd5507
jamesbartletr <james.bartlett.1993@icloud.com>
2022-06-11 16:19:23 +0100

    Edits to make package work

--- R/print_functions.R
+++ R/print_functions.R
@@ -1,13 +1,16 @@
+#' Print a Hello World message
 #' @export
 print_hello_world <- function(){
   print("Hello World!")
 }
 
+#' Print a Star Wars Theme message
+#' @param who should be a character from Star Wars
 #' @export
 print_star_wars <- function(who = "Obi Wan Kenobi"){
   if (who == "Obi Wan Kenobi"){
     print("Hello There!")
-  }else (who == "General Grievous"){
+  } else if (who == "General Grievous"){
     print("Ah, General Kenobi!")
   }
 }
