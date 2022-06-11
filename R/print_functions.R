cdf47299feb8f1e47cbfbd155bda35cb00b5c24b
jamesbartletr <james.bartlett.1993@icloud.com>
2022-06-11 19:20:30 +0100

    Additional functionality to allow the character's name to be included

--- R/print_functions.R
+++ R/print_functions.R
@@ -17,8 +17,11 @@ print_star_wars_greeting <- function(who = "Obi Wan Kenobi"){
 
 #' Print a random Star Wars quote
 #' @export
-print_star_wars_quote <- function(){
+print_star_wars_quote <- function(include_character=FALSE){
   quotes <- star_wars_quotes
-  selected_line <- sample(quotes$sw_quote, size = 1)
-  print(selected_line)
+  selected_line <- quotes[sample(1:nrow(quotes), size = 1),]
+  selected_quote <- ifelse(include_character,
+                           paste0(selected_line$sw_character,": ",selected_line$sw_quote),
+                           paste0(selected_line$sw_quote))
+  print(selected_quote)
 }
